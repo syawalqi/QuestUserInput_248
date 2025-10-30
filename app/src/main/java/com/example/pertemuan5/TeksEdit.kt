185a3e28e9a405acb8558d189d71efdb85fd3945
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-30 13:27:45 +0700

    menambahkan Column outlinedTextField Row Radiobutton pada file TeksEdit.kt

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -56,5 +57,12 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
                     ),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-    }
-}
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = { textJK = item }
+                    )
+                    Text(item)
+                }
+            }
+        }
+
