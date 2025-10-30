e47b66bd8fb29e677ff5b9b692329de6a7734d46
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-30 13:24:33 +0700

    menambahkan Column pada file TeksEdit.kt

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -1,11 +1,17 @@
 package com.example.pertemuan5
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier = Modifier) {
@@ -18,4 +24,12 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
     var jenis by remember { mutableStateOf("") }
 
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(
+        modifier = Modifier
+            .padding(top = 50.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 }
