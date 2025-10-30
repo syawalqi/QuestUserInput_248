91780340565e1a6c78dcb70a3fef962d6decc129
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-30 13:22:49 +0700

    menambahkan var teks pada file TeksEdit.kt

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -1,7 +1,15 @@
 package com.example.pertemuan5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+}
