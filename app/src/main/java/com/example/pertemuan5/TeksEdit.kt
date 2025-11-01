55c290ada9bc73fd6fe22cc1095efdf5b9c803dd
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-01 11:19:57 +0700

    menambah Text Jenis Kelamin

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -95,6 +95,24 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
                     label = { Text(text = "Nama Lengkap") },
                     onValueChange = { textNama = it }
                 )
+                Text("JENIS KELAMIN", fontWeight = FontWeight.SemiBold)
+                Row {
+                    gender.forEach { item ->
+                        Row(
+                            modifier = Modifier.selectable(
+                                selected = textJK == item,
+                                onClick = { textJK = item }
+                            ),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = textJK == item,
+                                onClick = { textJK = item }
+                            )
+                            Text(item)
+                        }
+                    }
+                }
 
             }
         }
