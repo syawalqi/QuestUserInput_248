3910b30d0c877290beaf1b31ff014f747023bc35
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-01 11:12:19 +0700

    menambah spacer

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -67,6 +68,8 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Bold
             )
         }
+
+        Spacer(modifier = Modifier.height(20.dp))
 
         OutlinedTextField(
             value = textNama,
