e67029e0b9dc8c8897cdaa69d758136eebb1bcaf
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-01 11:16:37 +0700

    menambah Column modifier

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -79,7 +80,10 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
             shape = RoundedCornerShape(16.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
         ){
-            Column ()
+            Column (
+                modifier = Modifier
+                    .padding(16.dp),
+            )
         }
 
 
